7c77d1ba1ca5a70b59bf595f935e8692c2e4341e
CLément Castex <ccastex@normandiewebschool.fr>
2024-09-19 10:06:59 +0200

    systeme de remplacement par un mot gentil fonctionnel

--- wp-content/plugins/clement-plug/plugin.php
+++ wp-content/plugins/clement-plug/plugin.php
@@ -3,9 +3,9 @@
  * @package Akismet
  */
 /*
-Plugin Name: Frendly Censure
+Plugin Name: Frendly Censure  
 Plugin URI: https://4each.fr/
-Description: A plugin that censors all inappropriate words, which you can customize as you like. It can be useful for automatic comment moderation. You can choose whether to replace words with a '*' or a more user-friendly censored message.
+Description: A plugin that censors all inappropriate words,( for exemple " crétin " )which you can customize as you like. It can be useful for automatic comment moderation. You can choose whether to replace words with a '*' or a more user-friendly censored message.
 Version: 5.3.3
 Requires at least: 5.8
 Requires PHP: 5.6.20
@@ -14,27 +14,69 @@ Author URI: https://automattic.com/wordpress-plugins/
 License: GPLv2 or later
 Text Domain: akismet
 */
-include_once plugin_dir_path(__FILE__) . './themes/clement/index.php';
+// include_once plugin_dir_path(__FILE__) . './themes/clement/index.php';
+function insererInsultesAvecMotsGentils() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'filter'; 
+
+    $insultes = array(
+        'grognard', 'tête de mule', 'imbécile', 'fainéant', 'crétin',
+        'responsable', 'borné', 'malotru', 'désagréable', 'horrible',
+        'détestable', 'tête de nœud', 'imbécile', 'navrant', 'fainéant', 'bras cassé', 'gamin', 'agaçant', 
+        'malotru', 'exécrable', 'fainéantise', 'méprisante', 'pestiféré', 'imbéciles', 'paresseux', 'peste', 
+        'crétin'
+    );
+
+    $mots_gentils = array(
+        'Amour', 'Étoile', 'Câlin', 'Chéri(e)', 'Soleil', 'Joie', 'Douceur', 'Bonheur', 'Harmonie', 
+        'Sourire', 'Paix', 'Lumière', 'Amical', 'Fleur', 'Trésor', 'Ange', 'Beauté', 'Bienveillance',
+        'Gentillesse', 'Compassion', 'Grâce', 'Générosité', 'Respect', 'Sérénité', 'Sympathie', 
+        'Espoir', 'Tendresse', 'Confiance', 'Amabilité', 'Rêveur', 'Plaisir', 'Splendeur', 'Brillant(e)', 
+        'Magie', 'Lumière', 'Sympathique', 'Radieux', 'Charmant(e)', 'Perle', 'Merveilleux', 'Calme', 
+        'Sagesse', 'Fantastique', 'Courageux', 'Héroïque', 'Inspirant', 'Talentueux', 'Fort(e)', 
+        'Délicatesse', 'Clarté', 'Fidèle', 'Vérité', 'Passionné', 'Divin(e)', 'Affectueux', 'Protecteur',
+        'Joyeux', 'Lumineux', 'Rafraîchissant', 'Pétillant', 'Optimiste', 'Positif', 'Créatif', 'Fabuleux',
+        'Enchanteur', 'Gracieux', 'Magnifique', 'Paisible', 'Doux', 'Sensible', 'Espiègle', 'Rêve', 
+        'Bienfaisant', 'Bienheureux', 'Ensoleillé', 'Éblouissant', 'Inoubliable', 'Passion', 'Émerveillé',
+        'Génie', 'Courage', 'Chaleureux', 'Attentionné', 'Loyal', 'Charmant', 'Adorable', 'Sérénité',
+        'Confiant', 'Motivé', 'Inspiré', 'Valeureux', 'Aimant', 'Passionné', 'Enthousiaste', 'Digne', 
+        'Loyal', 'Gentil', 'Doux', 'Souriant', 'Rayonnant'
+    );
+
+    $wpdb->query("TRUNCATE TABLE $table_name");
+
+    foreach ($insultes as $insulte) {
+        $mot_gentil = $mots_gentils[array_rand($mots_gentils)];
+        $wpdb->insert(
+            $table_name,
+            array(
+                'insultes' => $insulte,
+                'gentil' => $mot_gentil
+            )
+        );
+    }
+}
+function mon_plugin_activation() {
+    insererInsultesAvecMotsGentils();
+}
+register_activation_hook(__FILE__, 'mon_plugin_activation');
 
 function filtrerInsultes($texte) {
-    $insultes = array(' grognard', ' tête de mule ', ' imbécile ', ' fainéant ', ' crétin ', ' responsable ', ' borné ', ' malotru ', ' désagréable ', ' horrible '); 
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'filter';
+    $insultes = $wpdb->get_results( "SELECT insultes, gentil FROM $table_name", ARRAY_A );
+
     foreach ($insultes as $insulte) {
-        $texte = str_ireplace($insulte, str_repeat('¤', strlen($insulte)), $texte);
+        $texte = str_ireplace($insulte['insultes'], $insulte['gentil'], $texte);
     }
+
     return $texte;
 }
-// add_filter('the_content', 'filtrerInsultes');
+add_filter('the_content', 'filtrerInsultes', 20);
+add_filter('the_title', 'filtrerInsultes', 20);
+add_filter('widget_text', 'filtrerInsultes', 20);
+add_filter('gettext', 'filtrerInsultes', 20);
+add_filter('ngettext', 'filtrerInsultes', 20);
+add_filter('the_excerpt', 'filtrerInsultes', 20);
 
-
-function activer_plugin_filtrage_insultes() {
-
-};
-register_activation_hook(__FILE__, 'activer_plugin_filtrage_insultes');
-function desactiver_plugin_filtrage_insultes() {
-
-};
-register_deactivation_hook(__FILE__, 'desactiver_plugin_filtrage_insultes');
-
-
-//  var_dump ($phraseFiltrée); 
 ?>
--- wp-content/themes/clement/index.php
+++ wp-content/themes/clement/index.php
@@ -1,40 +1,19 @@
-<?php get_header();
-        include_once plugin_dir_path(__FILE__) . './plugins/clement-plug/plugin.php'; 
-        ?>
+<?php get_header();?>
 
+    
+    <p>Je ne comprends pas pourquoi tu es toujours aussi grognard et tête de mule. Franchement, c'est insupportable de te voir agir comme un imbécile à longueur de journée. Tu es vraiment stupide et fainéant, et tout le monde le voit. Sérieusement, arrête d'être un crétin et commence à te comporter comme une personne responsable. Il n'y a pas moyen de discuter avec toi sans que tu sois aussi borné et irrespectueux. C'est incroyable à quel point tu peux être un vrai malotru. On dirait que tu cherches constamment à être le plus désagréable possible, tu ne te rends même pas compte à quel point tu peux être méchant et horrible avec les autres.</p>
 
+        
+        <!-- // $texte = '<p>Je ne comprends pas pourquoi tu es toujours aussi grognard et tête de mule. Franchement, c\'est insupportable de te voir agir comme un imbécile à longueur de journée. Tu es vraiment stupide et fainéant, et tout le monde le voit. Sérieusement, arrête d\'être un crétin et commence à te comporter comme une personne responsable. Il n\'y a pas moyen de discuter avec toi sans que tu sois aussi borné et irrespectueux. C\'est incroyable à quel point tu peux être un vrai malotru. On dirait que tu cherches constamment à être le plus désagréable possible, tu ne te rends même pas compte à quel point tu peux être méchant et horrible avec les autres.</p>';
+        // preg_match('/<p>(.*?)<\/p>/', $texte, $matches);
+        
+        // $texteEntreBalises = $matches[1];
+        
+        // echo $texteEntreBalises;
 
+        // $texteEntreBalises = "Je ne comprends pas pourquoi tu es toujours aussi grognard et tête de mule. Franchement, c'est insupportable de te voir agir comme un imbécile à longueur de journée. Tu es vraiment stupide et fainéant, et tout le monde le voit. Sérieusement, arrête d'être un crétin et commence à te comporter comme une personne responsable. Il n'y a pas moyen de discuter avec toi sans que tu sois aussi borné et irrespectueux. C'est incroyable à quel point tu peux être un vrai malotru. On dirait que tu cherches constamment à être le plus désagréable possible, tu ne te rends même pas compte à quel point tu peux être méchant et horrible avec les autres.";
+        // $texteFiltrée = filtrerInsultes($texteEntreBalises);
+        // echo $texteFiltrée; -->
 
-<ul>
-    <?php while (have_posts()):
-        the_post(); ?>
-
-        <li>
-            <a href="<?php the_permalink(); ?>">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title"><?php the_title(); ?></h5>
-                        <p class="card-text"><?php the_content(); ?></p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </a>
-
-        </li>
-        <?php 
-      // function activer_plugin_filtrage_insultes() {
-        $phrase = "Je ne comprends pas pourquoi tu es toujours aussi grognard et tête de mule. Franchement, c'est insupportable de te voir agir comme un imbécile à longueur de journée. Tu es vraiment stupide et fainéant, et tout le monde le voit. Sérieusement, arrête d'être un crétin et commence à te comporter comme une personne responsable. Il n'y a pas moyen de discuter avec toi sans que tu sois aussi borné et irrespectueux. C'est incroyable à quel point tu peux être un vrai malotru. On dirait que tu cherches constamment à être le plus désagréable possible, tu ne te rends même pas compte à quel point tu peux être méchant et horrible avec les autres.";
-        $phraseFiltrée = filtrerInsultes($phrase);
-        echo $phraseFiltrée;
-      // }
-      // function desactiver_plugin_filtrage_insultes() {
-      //   $phrase = "Je ne comprends pas pourquoi tu es toujours aussi grognard et tête de mule. Franchement, c'est insupportable de te voir agir comme un imbécile à longueur de journée. Tu es vraiment stupide et fainéant, et tout le monde le voit. Sérieusement, arrête d'être un crétin et commence à te comporter comme une personne responsable. Il n'y a pas moyen de discuter avec toi sans que tu sois aussi borné et irrespectueux. C'est incroyable à quel point tu peux être un vrai malotru. On dirait que tu cherches constamment à être le plus désagréable possible, tu ne te rends même pas compte à quel point tu peux être méchant et horrible avec les autres.";
-      //   echo $phrase;
-      // }
-
-?>
-    <?php endwhile; ?>
-</ul>
 
 <?php get_footer(); ?>
